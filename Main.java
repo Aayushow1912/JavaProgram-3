b1c77bd439bd3773b7ef74436313ba32a1eed8e4
Aayush Tewari <113117171+Aayushow1912@users.noreply.github.com>
2024-08-21 10:17:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
-class Animal {
-    void eat() {
-        System.out.println("This animal eats food.");
+class Employee {
+    void quality() {
+        System.out.println("This is a good employee");
     }
 }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("The dog barks.");
+class Person extends Employee {
+    void habit() {
+        System.out.println("The person is also good");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.eat();  
-        myDog.bark(); 
+        Person obj = new Person();
+        obj.quality();  
+        obj.habit(); 
     }
 }
